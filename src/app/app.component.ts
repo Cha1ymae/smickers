857a17b1ae99052601682a298ffa1f6214be1fce
Chaymae Mechouahi <chaymaemechouahi@gmail.com>
2024-11-26 15:43:14 +0100

    CMI:Add Product component logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from './footer/footer.component';
 import { ProductComponent } from './product/product.component';
+import { Product } from './product/product.types';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,13 @@ import { ProductComponent } from './product/product.component';
 })
 export class AppComponent {
   title = 'smickers';
+  product: Product = 
+    {
+      id: '1',
+      title: 'Basketball Shoes',
+      description: 'A pair of comfortable and stylish basketball shoes.',
+      photo: '',
+      price: 120,
+      stock: 99
+    }
 }
