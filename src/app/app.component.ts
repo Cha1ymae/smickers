b5f895e6e53214c9b8a91f65cf18082cafc490a9
Ashar <sisiiraa.m@gmail.com>
2024-11-26 17:06:07 +0100

    1st product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductComponent } from './product/product.component';
 import { Product } from './product/product.types';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, FooterComponent, HeaderComponent, ProductComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'smickers';
-  product: Product = 
-    {
-      id: '1',
-      title: 'Basketball Shoes',
-      description: 'A pair of comfortable and stylish basketball shoes.',
-      photo: '',
-      price: 120,
-      stock: 99
-    }
+  product: Product = {
+    id: '1',
+    title: 'Air Force 1',
+    description: 'A pair of comfortable and stylish basketball shoes.',
+    photo: 'https://images.laced.com/products/28ceea49-a3ef-40a7-81db-3c1dc68f3233.jpg?auto=format&fit=max&w=750&q=75', 
+    price: 120,
+    stock: 99
+  }; 
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { importProvidersFrom } from '@angular/core';
 
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-  provideRouter(routes), 
-  provideAnimations(),
-  
-  
-],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+  ],
 };
