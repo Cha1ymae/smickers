7587bd544b751243aba3d01712e7bd88cd4031ba
Chaymae Mechouahi <chaymaemechouahi@gmail.com>
2024-12-02 21:33:53 +0100

    CMI:Add sneakers

---
+++ src/app/sneakers/sneakers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sneakers',
+  imports: [],
+  templateUrl: './sneakers.component.html',
+  styleUrl: './sneakers.component.css'
+})
+export class SneakersComponent {
+
+}
