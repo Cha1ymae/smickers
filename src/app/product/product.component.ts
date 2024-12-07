63086ac05afe583240873f1c6d0b4c3a00ab5b8c
Chaymae Mechouahi <chaymaemechouahi@gmail.com>
2024-12-07 21:51:24 +0100

    CMI: Commit all changes

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,50 +1,51 @@
-import { Component, Input } from '@angular/core';
-import { UpperCasePipe, NgIf, NgStyle,CurrencyPipe  } from '@angular/common';
+import { Component, Input, inject } from '@angular/core';
+import { UpperCasePipe, NgIf, NgStyle, CurrencyPipe } from '@angular/common';
 import { Product } from './product.types';
 
 @Component({
   selector: 'app-product',
-  imports: [NgStyle,NgIf,UpperCasePipe , CurrencyPipe ],
+  imports: [NgStyle, NgIf, UpperCasePipe, CurrencyPipe],
   template: `
     <div class="product">
-      <img 
-      [src]=productImage
-        class="product-image" 
-        alt="{{ product?.title }}" 
+      <img
+        [src]="productImage"
+        class="product-image"
+        alt="{{ product?.title }}"
       />
       <div class="product-info">
-        <h1>{{ product?.title | uppercase }} </h1>
-        <h2>{{ product?.price | currency:'EUR':'symbol' }}</h2>
+        <h1>{{ product?.title | uppercase }}</h1>
+        <h2>{{ product?.price | currency : 'EUR' : 'symbol' }}</h2>
         <p>{{ product?.description }}</p>
       </div>
-      <button 
-      class="add-to-cart"
-      *ngIf="product?.stock != 0" 
-      [disabled]="product?.stock === 0"   
-      (click)="onAddToCart()"
-      [ngStyle]="{ 
-              'background-color': addedToCart ? 'green' : 'red',
-              'color': 'white'  }">
-  {{ addedToCart ? 'Ajouté au panier' : 'Ajouter au panier' }}
-</button>
+      <button
+        class="add-to-cart"
+        *ngIf="product?.stock != 0"
+        [disabled]="product?.stock === 0"
+        (click)="onAddToCart()"
+        [ngStyle]="{
+          'background-color': addedToCart ? 'green' : 'red',
+          color: 'white'
+        }"
+      >
+        {{ addedToCart ? 'Ajouté au panier' : 'Ajouter au panier' }}
+      </button>
     </div>
   `,
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent {
   @Input() product?: Product;
   addedToCart: boolean = false;
-
   onAddToCart() {
-    this.addedToCart = true; 
-    console.log('Produit ajouté au panier',this.product?.title);
+    this.addedToCart = true;
+    console.log('Produit ajouté au panier', this.product?.title);
     setTimeout(() => {
       this.addedToCart = false;
     }, 5000);
   }
   get productImage(): string {
-    return this.product?.photo?.trim() 
-      ? this.product.photo 
+    return this.product?.photo?.trim()
+      ? this.product.photo
       : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0Y2SLfdkXPJMxZ2QtRIK1fy2Lvnr6UVgmzQ&s';
   }
 }
