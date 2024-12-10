a834f4e852f7622dfa812364c0652afd864f2890
sisiira <sisiiraa.m@gmail.com>
2024-12-10 12:06:38 +0100

    Signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component,OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { Component, OnInit, signal, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product/product.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { SeasonCarouselComponent } from "./season-carousel/season-carousel.component";
+import { SeasonCarouselComponent } from './season-carousel/season-carousel.component';
 import { MyProductService } from './my-product.service';
-import { NewsLetterComponent } from "./news-letter/news-letter.component";
+import { NewsLetterComponent } from './news-letter/news-letter.component';
+import { Product } from './product/product.types';
 
 @Component({
   selector: 'app-root',
@@ -17,29 +18,35 @@ import { NewsLetterComponent } from "./news-letter/news-letter.component";
     ProductComponent,
     SeasonCarouselComponent,
     NewsLetterComponent
-],
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
- title = 'smickers';
- protected myProducts = new Array();
- selectedCategory: string = '';
- constructor(private myProductService : MyProductService) {}
- 
- ngOnInit(): void {
-  this.myProductService
-  .getProducts().subscribe((response) => this.myProducts = response.data);
-}
+  title = 'smickers';
 
- onSeasonChanged(season: string) {
-  this.selectedCategory = season;
-}
+  myProducts = signal<Product[]>([]);
 
-scrollToProducts(): void {
-  const productSection = document.getElementById('product-section');
-  if (productSection) {
-    productSection.scrollIntoView({ behavior: 'smooth' });
+  inStock = computed(() => this.myProducts().filter((product) => product.stock > 0));
+
+  selectedCategory: string = '';
+
+  constructor(private myProductService: MyProductService) {}
+
+  ngOnInit(): void {
+    this.myProductService.getProducts().subscribe((response) => {
+      this.myProducts.set(response.data);
+    });
   }
-}
+
+  onSeasonChanged(season: string): void {
+    this.selectedCategory = season;
+  }
+
+  scrollToProducts(): void {
+    const productSection = document.getElementById('product-section');
+    if (productSection) {
+      productSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
